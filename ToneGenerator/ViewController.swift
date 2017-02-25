e0bbfd16fcae5f3c43c3fea593b0d1c095cc78e6
14libb <14libb@gmail.com>
2017-02-25 13:52:10 -0500

    microphone reading and get data from Betsy

--- ToneGenerator/ViewController.swift
+++ ToneGenerator/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 import AVFoundation
 
+import AVFoundation
+import CoreAudio
+
+var recorder: AVAudioRecorder!
+var levelTimer = Timer()
+var lowPassResults: Double = 0.0
+
 class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
@@ -48,7 +55,114 @@ class ViewController: UIViewController {
         
         
         //microphone initialization
+        do {
+        //make an AudioSession, set it to PlayAndRecord and make it active
+        let audioSession:AVAudioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try audioSession.setActive(true)
+        
+        //set up the URL for the audio file
+        let documents: NSString = NSSearchPathForDirectoriesInDomains( FileManager.SearchPathDirectory.documentDirectory,  FileManager.SearchPathDomainMask.userDomainMask, true)[0] as NSString
+        let str =  documents.appendingPathComponent("recordTest.caf")
+        let url = NSURL.fileURL(withPath: str as String)
+        
+        // make a dictionary to hold the recording settings so we can instantiate our AVAudioRecorder
+            let recordSettings:[String: Any] = [(AVFormatIDKey as NSObject) as! String:kAudioFormatAppleIMA4 as AnyObject,
+                                                      (AVSampleRateKey as NSObject) as! String:44100.0 as AnyObject,
+                                                      (AVNumberOfChannelsKey as NSObject) as! String:2 as AnyObject,(AVEncoderBitRateKey as NSObject) as! String:12800 as AnyObject,
+                                                      (AVLinearPCMBitDepthKey as NSObject) as! String:16 as AnyObject,
+                                                      (AVEncoderAudioQualityKey as NSObject) as! String:AVAudioQuality.max.rawValue as AnyObject
+            
+        ]
+        
+        //declare a variable to store the returned error if we have a problem instantiating our AVAudioRecorder
+        var error: NSError?
+        
+        //Instantiate an AVAudioRecorder
+        try recorder = AVAudioRecorder(url: url, settings: recordSettings)
+        //recorder = AVAudioRecorder(URL:url, settings: recordSettings, error: &error)
+        //If there's an error, print that shit - otherwise, run prepareToRecord and meteringEnabled to turn on metering (must be run in that order)
+        if let e = error {
+            NSLog(e.localizedDescription)
+        } else {
+            recorder.prepareToRecord()
+            recorder.isMeteringEnabled = true
+            
+            //start recording
+            recorder.record()
+            
+            //instantiate a timer to be called with whatever frequency we want to grab metering values
+            levelTimer = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector(ViewController.levelTimerCallback), userInfo: nil, repeats: true)
+            
+        }
+        } catch {
+            
+        }
     }
+    
+    //This selector/function is called every time our timer (levelTime) fires
+    func levelTimerCallback() {
+        //NSLog("here");
+        //we have to update meters before we can get the metering values
+        recorder.updateMeters()
+        
+        //print to the console if we are beyond a threshold value. Here I've used -7
+        //if recorder.averagePower(forChannel: 0) > -7 {
+        //NSLog("%f", recorder.averagePower(forChannel: 0))
+        
+        //if we're blowing
+        let vol = recorder.averagePower(forChannel: 0)
+        if (vol > -10) {
+            //NSLog("exceeded")
+            //do something because we're blowing
+            //make the request
+            let urlPath: String = "https://api.particle.io/v1/devices/3e0033000a47353138383138/status?access_token=2a62610028c4a017bcea1bddec41439585c23a9b"
+            let url: NSURL = NSURL(string: urlPath)!
+            //let request = NSURLRequest(url: url as URL)
+            //request.httpMethod = "GET"
+            
+            let session = URLSession.shared
+            //session.dataTaskWithUrl(with: request as URLRequest) { (data)
+            let task = session.dataTask(with: url as URL) { (data, response, error) -> Void in
+                
+                if (error != nil) {
+                    NSLog((error as! NSString) as String)
+                } else {
+                    // Handle incoming data like you would in synchronous request
+                    let reply = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                    if let data = reply?.data(using: String.Encoding.utf8.rawValue) {
+                        
+                        do {
+                            let dictonary = try JSONSerialization.jsonObject(with: data, options: []) as? [String:AnyObject]
+                            
+                            if let myDictionary = dictonary
+                            {
+                                print(" First name is: \(myDictionary["result"]!)")
+                            }
+                        } catch let error as NSError {
+                            print(error)
+                        }
+                    }
+                }
+                
+            }
+            task.resume()
+
+        }
+        
+        
+    }
+    func convertToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
