1e278ee1691029071a06ce0db74b69e6b03240a9
14libb <alex.ringlein@gmail.com>
2017-02-25 23:21:56 -0500

    final commit

--- ToneGenerator/AVTonePlayerUnit.swift
+++ ToneGenerator/AVTonePlayerUnit.swift
@@ -16,6 +16,8 @@ class AVTonePlayerUnit: AVAudioPlayerNode {
     
     var frequency: Double = 440.0
     var amplitude: Double = 0.5
+    var mod: Float = 0
+    var val: Float = 0.0
     
     private var theta: Double = 0.0
     private var audioFormat: AVAudioFormat!
@@ -31,6 +33,26 @@ class AVTonePlayerUnit: AVAudioPlayerNode {
         return buffer
     }
     
+    func flat(val: Float) -> Float {
+        if theta < M_PI {
+            return 1.0
+        } else {
+            return -1.0
+        }
+    }
+    
+    func pointy(val: Float) -> Float {
+        if theta >= 0 && theta < M_PI/2 {
+            return Float(theta)/Float((M_PI/2))
+        } else if theta >= M_PI/2 && theta < M_PI {
+            return Float((1.0 - (theta - M_PI/2) / (M_PI/2)))
+        } else if theta > M_PI && theta <= M_PI * 3.0 / 2.0{
+            return Float((theta - M_PI) / (M_PI/2) * (-1.0))
+        } else {
+            return Float((1.0 - (theta - 3 * M_PI/2) / (M_PI/2)) * (-1.0))
+        }
+    }
+    
     func fillBuffer(_ buffer: AVAudioPCMBuffer) {
         let data = buffer.floatChannelData?[0]
         let numberFrames = buffer.frameCapacity
@@ -38,7 +60,14 @@ class AVTonePlayerUnit: AVAudioPlayerNode {
         let theta_increment = 2.0 * M_PI * self.frequency / self.sampleRate;
         
         for frame in 0..<Int(numberFrames) {
-            data?[frame] = Float32(sin(theta) * amplitude)
+            var value: Float
+            if mod < 0 {
+                 value = Float(sin(theta)) * Float(amplitude) * (1 - (mod*(-1))) + Float(pointy(val: Float(theta))) * Float(amplitude) * (mod * (-1))
+            } else {
+                 value = Float(sin(theta)) * Float(amplitude) * (1 - mod) + Float(flat(val: Float(theta))) * Float(amplitude) * mod
+            }
+            val = value
+            data?[frame] = Float32(val)
             
             theta += theta_increment
             if theta > 2.0 * M_PI {
--- ToneGenerator/ViewController.swift
+++ ToneGenerator/ViewController.swift
@@ -12,16 +12,26 @@ import AVFoundation
 import AVFoundation
 import CoreAudio
 
+import CoreBluetooth
+
 var recorder: AVAudioRecorder!
 var levelTimer = Timer()
 var lowPassResults: Double = 0.0
 
-class ViewController: UIViewController {
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var amplitudeSlider: UISlider!
-    
-    @IBOutlet weak var otherLabel: UILabel!
+let BEAN_NAME = "Robu"
+let BEAN_SCRATCH_UUID =
+    CBUUID(string: "a495ff21-c5b1-4b44-b512-1370f02d74de")
+let BEAN_SERVICE_UUID =
+    CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
+
+let screenSize: CGRect = UIScreen.main.bounds
+
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+//    @IBOutlet weak var slider: UISlider!
+//    @IBOutlet weak var label: UILabel!
+//    @IBOutlet weak var amplitudeSlider: UISlider!
+//    
+//    @IBOutlet weak var otherLabel: UILabel!
     
     //music playing
     var engine: AVAudioEngine!
@@ -29,24 +39,51 @@ class ViewController: UIViewController {
     
     var frequency: Float!
     var partial: Int!
+    var frequencyMod: Float!
+    var mod: Float!
+    
+    var x: Float!
+    var y: Float!
+    
+    var imageView: UIImageView!
+    var imageSize: CGSize!
+    
+    var loudness: [Float]!
+    
+    
+    //bluetooth
+    var manager:CBCentralManager!
+    var peripheral:CBPeripheral!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
+        manager = CBCentralManager(delegate: self, queue: nil)
+
+        
         self.frequency = 410
         self.partial = 0
+        self.frequencyMod = 0.0
+        self.mod = 0
+        self.x = 0.0
+        self.y = 0.0
+        self.loudness = []
        //tone player setup
         tone = AVTonePlayerUnit()
         tone.volume = 1;
-        label.text = String(format: "%.1f", tone.frequency)
-        slider.minimumValue = -5.0
-        slider.maximumValue = 5.0
-        slider.value = 0.0
-        
-        amplitudeSlider.minimumValue = 0
-       amplitudeSlider.maximumValue = 1
-        amplitudeSlider.value = 0.25
+//        label.text = String(format: "%.1f", tone.frequency)
+//        slider.minimumValue = -5.0
+//        slider.maximumValue = 5.0
+//        slider.value = 0.0
+//        
+//        slider.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_2));
+//        
+//        amplitudeSlider.minimumValue = 0
+//       amplitudeSlider.maximumValue = 1
+//        amplitudeSlider.value = 0.25
         let format = AVAudioFormat(standardFormatWithSampleRate: tone.sampleRate, channels: 1)
         print(format.sampleRate)
         engine = AVAudioEngine()
@@ -71,6 +108,7 @@ class ViewController: UIViewController {
             let str =  documents.appendingPathComponent("recordTest.caf")
             let url = NSURL.fileURL(withPath: str as String)
             
+            print("1")
             // make a dictionary (disgusting)
                 let recordSettings:[String: Any] = [(AVFormatIDKey as NSObject) as! String:kAudioFormatAppleIMA4 as AnyObject,
                                                           (AVSampleRateKey as NSObject) as! String:44100.0 as AnyObject,
@@ -79,13 +117,13 @@ class ViewController: UIViewController {
                                                           (AVEncoderAudioQualityKey as NSObject) as! String:AVAudioQuality.max.rawValue as AnyObject
                 
             ]
-            
+            print("2")
             var error: NSError?
             
             //Instantiate an AVAudioRecorder
             try recorder = AVAudioRecorder(url: url, settings: recordSettings)
             if let e = error {
-                NSLog(e.localizedDescription)
+                //NSLog(e.localizedDescription)
             } else {
                 recorder.prepareToRecord()
                 recorder.isMeteringEnabled = true
@@ -97,20 +135,220 @@ class ViewController: UIViewController {
         } catch {
             //whoops
         }
+        
+        imageSize = CGSize(width: screenSize.width, height: screenSize.height)
+        imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: imageSize))
+        self.view.addSubview(imageView)
+        
+    
+}
+
+func drawCustomImage(size: CGSize) -> UIImage {
+    print("drawing")
+    // Setup our context
+    let bounds = CGRect(origin: CGPoint.zero, size: size)
+    let opaque = false
+    let scale: CGFloat = 0
+    UIGraphicsBeginImageContextWithOptions(size, opaque, scale)
+    let context = UIGraphicsGetCurrentContext()
+    
+    // Setup complete, do drawing here
+    let color = UIColor.red
+    context!.setStrokeColor(color.cgColor)
+    context!.setLineWidth(1.0)
+    
+    context!.stroke(bounds)
+    
+    context!.beginPath()
+    if loudness.count <= Int(screenSize.width) {
+        for i in 0..<loudness.count {
+            context?.move(to: CGPoint(x: i, y: (Int(loudness[i]) + 40) * -4 + Int(screenSize.height)/2))
+            context?.addLine(to: CGPoint(x:i, y:(Int(loudness[i]) + 40) * 4 + Int(screenSize.height)/2))
+        }
+        context!.strokePath()
+    
+    } else {
+        let diff = loudness.count - Int(screenSize.width)
+        for i in (loudness.count - Int(screenSize.width))..<loudness.count {
+            context?.move(to: CGPoint(x: i - diff , y: (Int(loudness[i]) + 40) * -4 + Int(screenSize.height)/2))
+            context?.addLine(to: CGPoint(x:i - diff , y:(Int(loudness[i]) + 40) * 4 + Int(screenSize.height)/2))
+        }
+        context!.strokePath()
     }
+    context!.strokePath()
+    
+    // Drawing complete, retrieve the finished image and cleanup
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return image!
+}
+
+    
+//    func draw(_ rect: CGRect) {
+//        let aPath = UIBezierPath()
+//        
+//        aPath.move(to: CGPoint(x:20, y:50))
+//        
+//        aPath.addLine(to: CGPoint(x:300, y:50))
+//        
+//        //Keep using the method addLineToPoint until you get to the one where about to close the path
+//        
+//        aPath.close()
+//        
+//        //If you want to stroke it with a red color
+//        UIColor.red.set()
+//        aPath.stroke()
+//        //If you want to fill it as well
+//        aPath.fill()
+//    }
+    
+    //potentially touch screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.location(in: self.view)
+            print(position.x)
+            print(position.y)
+            self.x = Float(position.x)
+            self.y = Float(position.y)
+        }
+        
+}
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+        if let touch = touches.first {
+            let position = touch.location(in: self.view)
+            print(position.x)
+            print(position.y)
+            updateMods(x: position.x, y: position.y)
+        }
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.frequencyMod = 0.0
+        self.mod = 0
+    }
+    
+    func updateMods(x: CGFloat, y: CGFloat) {
+        let xfrac = (x - CGFloat(self.x)) / screenSize.width
+        let yfrac = (y - CGFloat(self.y)) / screenSize.height
+        
+        self.frequencyMod = (Float(yfrac)) + self.frequencyMod
+        self.mod = Float(xfrac) + self.mod
+        if self.mod > 1 {
+            self.mod = 1.0
+        } else if self.mod < -1.0 {
+            self.mod = -1.0
+        }
+        
+        self.x = Float(x)
+        self.y = Float(y)
+        
+    }
+    
+    //bluetooth
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if #available(iOS 10.0, *) {
+            if central.state == CBManagerState.poweredOn {
+                print("scanning for peripherals")
+                central.scanForPeripherals(withServices: nil, options: nil)
+            } else {
+                print("Bluetooth not available.")
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("found device")
+        
+        let device = (advertisementData as NSDictionary)
+            .object(forKey: CBAdvertisementDataLocalNameKey)
+            as? NSString
+        
+        print(device)
+        
+        if device?.contains(BEAN_NAME) == true {
+            self.manager.stopScan()
+            
+            self.peripheral = peripheral
+            self.peripheral.delegate = self
+            
+            manager.connect(peripheral, options: nil)
+        }
+
+    }
+    
+    
+    func centralManager(
+        central: CBCentralManager,
+        didConnectPeripheral peripheral: CBPeripheral) {
+        peripheral.discoverServices(nil)
+    }
+    
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didDiscoverServices error: Error?) {
+        for service in peripheral.services! {
+            let thisService = service as CBService
+            
+            if service.uuid == BEAN_SERVICE_UUID {
+                peripheral.discoverCharacteristics(
+                    nil,
+                    for: thisService
+                )
+            }
+        }
+    }
+    
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didDiscoverCharacteristicsFor service: CBService,
+        error: Error?) {
+        for characteristic in service.characteristics! {
+            let thisCharacteristic = characteristic as CBCharacteristic
+            
+            if thisCharacteristic.uuid == BEAN_SCRATCH_UUID {
+                self.peripheral.setNotifyValue(
+                    true,
+                    for: thisCharacteristic
+                )
+            }
+        }
+    }
+    
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didUpdateValueFor characteristic: CBCharacteristic,
+        error: Error?) {
+        var count:UInt32 = 0;
+        
+        if characteristic.uuid == BEAN_SCRATCH_UUID {
+            //characteristic.value!.copyBytes(to: &count, count: UInt32(MemoryLayout<UInt32>.size))
+            //labelCount.text =
+            //count = characteristic.value as Int
+             //   NSString(format: "%llu", count) as String
+            print("got a matching id")
+        }
+    }
+    
     
     //This called every time timer fires
     func levelTimerCallback() {
+        let image = drawCustomImage(size: imageSize)
+        imageView.image = image
         recorder.updateMeters()
         
         //if we're blowing
         let vol = recorder.averagePower(forChannel: 0)
-        if (vol > -20) {
+        self.loudness.append(vol)
+        if (vol > -23) {
             if (vol > -10) {
-                print("high")
+                //print("high")
                 self.partial = 1
             } else {
-                print("blowing");
+                //print("blowing");
                 self.partial = 0
             }
             
@@ -138,34 +376,48 @@ class ViewController: UIViewController {
                             if let myDictionary = dictonary
                             {
                                 let state = myDictionary["result"] as! String
+                                
+                                if self.partial == 0 || self.partial == 1 {
+                                    switch state {
+                                    case "open" : self.frequency = 440
+                                    case "valveOne" : self.frequency = 493.88
+                                    case "valveTwo" : self.frequency = 554.37 // C#
+                                    case "valveThree": self.frequency = 587.33 // D
+                                    case "valveTwoThree": self.frequency = 659.25
+                                    case "valveOneThree": self.frequency = 739.99
+                                    case "valveOneTwo" : self.frequency = 830.61
+                                    case "valveOneTwoThree": self.frequency = 880 
+                                    default : self.frequency = 1
+                                    }
+                                }
                                 //first partial
-                                if self.partial == 0 {
-                                    switch state {
-                                    case "open" : self.frequency = 523.5 //C
-                                    case "valveOne" : self.frequency = 466.16 //Bb
-                                    case "valveTwo" : self.frequency = 493.88 // B
-                                    case "valveThree": self.frequency = 415.3 // Ab
-                                    case "valveTwoThree": self.frequency = 622.25 // Eb
-                                    case "valveOneThree": self.frequency = 587.33 // D
-                                    case "valveOneTwo" : self.frequency = 440 // A
-                                    case "valveOneTwoThree": self.frequency = 554.37 // C#
-                                    default : self.frequency = 1
-                                    }
-                                }
+//                                if self.partial == 0 || self.partial == 1 {
+//                                    switch state {
+//                                    case "open" : self.frequency = 523.5 //C
+//                                    case "valveOne" : self.frequency = 466.16 //Bb
+//                                    case "valveTwo" : self.frequency = 493.88 // B
+//                                    case "valveThree": self.frequency = 415.3 // Ab
+//                                    case "valveTwoThree": self.frequency = 622.25 // Eb
+//                                    case "valveOneThree": self.frequency = 587.33 // D
+//                                    case "valveOneTwo" : self.frequency = 440 // A
+//                                    case "valveOneTwoThree": self.frequency = 554.37 // C#
+//                                    default : self.frequency = 1
+//                                    }
+//                                }
                                 //second partial
-                                if self.partial == 1 {
-                                    switch state {
-                                    case "open" : self.frequency = 783.99 // G
-                                    case "valveOne" : self.frequency = 698.46 //F                              
-                                    case "valveTwo" : self.frequency = 739.99 // F#
-                                    case "valveThree": self.frequency = 880 // A
-                                    case "valveTwoThree": self.frequency = 830.61// G#
-                                    case "valveOneThree": self.frequency = 932.33 // A#
-                                    case "valveOneTwo" : self.frequency = 659.25 // E
-                                    case "valveOneTwoThree": self.frequency = 987.77 // B
-                                    default : self.frequency = 1
-                                    }
-                                }
+//                                if self.partial == 1 {
+//                                    switch state {
+//                                    case "open" : self.frequency = 783.99 // G
+//                                    case "valveOne" : self.frequency = 698.46 //F                              
+//                                    case "valveTwo" : self.frequency = 739.99 // F#
+//                                    case "valveThree": self.frequency = 880 // A
+//                                    case "valveTwoThree": self.frequency = 830.61// G#
+//                                    case "valveOneThree": self.frequency = 932.33 // A#
+//                                    case "valveOneTwo" : self.frequency = 659.25 // E
+//                                    case "valveOneTwoThree": self.frequency = 987.77 // B
+//                                    default : self.frequency = 1
+//                                    }
+//                                }
                                 
                             }
                             
@@ -178,7 +430,8 @@ class ViewController: UIViewController {
                 
             }
             task.resume()
-            tone.frequency = Double(frequency)
+            tone.frequency = Double(frequency) + Double(frequencyMod) * 40.0
+            tone.mod = self.mod
             
             if !tone.isPlaying {
                 tone.preparePlaying()
@@ -188,12 +441,13 @@ class ViewController: UIViewController {
 
         } else {
             self.partial = 0
-            print("not blowing")
+            //print("not blowing")
             if tone.isPlaying {
                 engine.mainMixerNode.volume = 0.0
                 tone.stop()
             }
         }
+
         
         
     }
@@ -214,31 +468,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func sliderChanged(_ sender: UISlider) {
-        
-        if (sender.tag == 0) {
-            let freq = 440.0 * pow(2.0, Double(slider.value))
-            tone.frequency = freq
-            label.text = String(format: "%.1f", freq)
-                    } else {
-            let amp = Double(sender.value);
-            tone.amplitude = Double(amp);
-            otherLabel.text = String(format: "%f.1f", amp);
-        }
-        
-    }
+//    @IBAction func sliderChanged(_ sender: UISlider) {
+//        
+//        if (sender.tag == 0) {
+//            let freq = 440.0 * pow(2.0, Double(slider.value))
+//            tone.frequency = freq
+//            label.text = String(format: "%.1f", freq)
+//                    } else {
+//            let amp = Double(sender.value);
+//            tone.amplitude = Double(amp);
+//            otherLabel.text = String(format: "%f.1f", amp);
+//        }
+//        
+//    }
     
-    @IBAction func togglePlay(_ sender: UIButton) {
-        if tone.isPlaying {
-            engine.mainMixerNode.volume = 0.0
-            tone.stop()
-            sender.setTitle("Start", for: UIControlState())
-        } else {
-            tone.preparePlaying()
-            tone.play()
-            engine.mainMixerNode.volume = 1.0
-            sender.setTitle("Stop", for: UIControlState())
-        }
-    }
+//    @IBAction func togglePlay(_ sender: UIButton) {
+//        if tone.isPlaying {
+//            engine.mainMixerNode.volume = 0.0
+//            tone.stop()
+//            sender.setTitle("Start", for: UIControlState())
+//        } else {
+//            tone.preparePlaying()
+//            tone.play()
+//            engine.mainMixerNode.volume = 1.0
+//            sender.setTitle("Stop", for: UIControlState())
+//        }
+//    }
 }
 
